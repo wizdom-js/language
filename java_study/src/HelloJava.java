d2133507a259af4affffe03574fd3913e4bb2bc9
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2021-01-12 00:00:43 +0900

    created java_study

---
+++ java_study/src/HelloJava.java
@@ -0,0 +1,13 @@
+// 자바 : 프로그래밍 언어이며, 컴퓨터 프로그램을 만드는데 사용한다.
+// 실행환경(JRE) + 개발도구(JDK) + 라이브러리(API)
+
+
+
+
+
+
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.println("안녕 자");
+    }
+}
