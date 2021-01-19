894763122222ed9e3b40b2dfa2700fba1c63f873
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2021-01-19 23:54:14 +0900

    연산자

---
+++ java_study/src/Operator.java
@@ -0,0 +1,28 @@
+public class Operator {
+    public static void main(String[] args) {
+        // 몫과 나머지 구하기
+        System.out.println(6/4);
+        System.out.println(7%3);
+
+        // 대입 연산자 및 자료형 변환
+        int number = 5;
+        number = (int)1.2;
+        System.out.println(number);
+
+        // 다른 자료형끼리의 계산
+        System.out.println(1 + 1.2);
+        System.out.println(7 / 2.0);
+
+        // 비교 연산자
+        System.out.println(2 < 3);
+        System.out.println(2 <= 3);
+        System.out.println(2 > 3);
+        System.out.println(3 >= 3);
+        System.out.println(5 == 5);
+        System.out.println(2 == 3);
+        System.out.println(2 != 3);
+        System.out.println(3 != 3);
+
+
+    }
+}
