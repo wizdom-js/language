5f3ea20d49960a75f4c2761e9e3d8118cef40240
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2021-03-30 23:56:59 +0900

    배열

---
+++ java_study/src/ArrayExample.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        String[] days = {"월", "화", "수", "목", "금", "토", "일"};
+
+        String[] days2 = Arrays.copyOf(days, 8);
+
+        days2[7] = "일";
+
+        for (String day : days2) {
+            System.out.println(day);
+        }
+    }
+}
